5170fd0fd2a058085864e290c326511f0292cfec
Perdro Victor <pvmcabral2005@gmail.com>
2026-07-03 11:36:07 -0300

    Listas python

---
+++ listas.py
@@ -0,0 +1,21 @@
+#lista = [10, 5, 25, 30]
+#print(sum(lista))
+#print(lista[1])
+#lista.append(50)
+
+#nomes = ['Maria','João','Tom']
+#for i in range(3):
+    #nome = input("Digite o nome:")
+    #nomes.append(nome)
+    #print(nomes)
+
+numeros = [20,8,35,7,9,8,40]
+print(len(numeros))
+print(numeros[-2])
+print(numeros[4])
+numeros.extend([18,24])
+print(numeros)
+numeros.pop(1)
+print(numeros)
+print(sorted(numeros))
+print(numeros.count(8))

c09146723e230001784229cf491f73fc1913ac20
Perdro Victor <pvmcabral2005@gmail.com>
2026-07-17 11:25:17 -0300

    arquivos

--- Teste/arquivos.py
+++ Teste/arquivos.py
@@ -1,3 +1,19 @@
-arquivo = open("exemplo.txt", "a", encoding = "utf-8")
-arquivo.write("História de diferentes espécies de animais que convivem juntos")
-print(arquivo)
+def arquivo_leitura(ler_arquivo):
+    arquivo = open(ler_arquivo, "r")
+    conteudo = arquivo.read()
+    print("conteúdo do arquivo:")
+    print(conteudo)
+    arquivo.close()
+
+def adicionar_animal(ler_arquivo,animal):
+    arquivo = open(ler_arquivo, "a")
+    arquivo.write("\n" + animal)
+    arquivo.close()
+    
+ler_arquivo = "exemplo.txt"
+arquivo_leitura(ler_arquivo)
+
+animal = input("Digite o animal que quer adicionar")
+adicionar_animal(ler_arquivo, animal)
+print("Arquivo atualizado")
+print(ler_arquivo)
